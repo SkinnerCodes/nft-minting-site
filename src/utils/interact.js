73f16a30eea69046d38288e51809fb69b48a3a53
Skinner <n/a>
2023-10-29 23:23:37 -0400

    site implementation

---
+++ src/utils/interact.js
@@ -0,0 +1,136 @@
+import Web3 from 'web3';
+import contractABI from '../MyNFT.json'
+const web3 = new Web3(window.ethereum)
+// Check if the connection is successful
+web3.eth.net.isListening()
+  .then(() => console.log('Connected to sepolia network'))
+  .catch(e => console.log('Something went wrong! ' + e));
+
+const contractAddress = "0x82769B1D2a5633fD941852BecF0C31d70a405b0b";
+
+export const mintNFT = async(url, name, description) => {
+
+    //error handling
+    if (url.trim() == "" || (name.trim() == "" || description.trim() == "")) { 
+        return {
+            success: false,
+            status: "❗Please make sure all fields are completed before minting.",
+        }
+    }
+
+   //make metadata
+    const metadata = {
+        name: name,
+        image: url,
+        description: description,
+    };
+    const jsonString = JSON.stringify(metadata);
+    const tokenURI = `data:application/json,${encodeURIComponent(jsonString)}`;
+
+    //load smart contract
+    console.log(web3.currentProvider)
+    window.contract = await new web3.eth.Contract(contractABI.abi, contractAddress);//loadContract();
+
+    //set up your Ethereum transaction
+    const transactionParameters = {
+        to: contractAddress, // Required except during contract publications.
+        from: window.ethereum.selectedAddress, // must match user's active address.
+        'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress, tokenURI).encodeABI() //make call to NFT smart contract 
+    };
+
+    //sign transaction via Metamask
+    try {
+        const txHash = await window.ethereum
+            .request({
+                method: 'eth_sendTransaction',
+                params: [transactionParameters],
+            });
+        return {
+            success: true,
+            status: "✅ Check out your transaction on Etherscan: https://sepolia.etherscan.io/tx/" + txHash
+        }
+    } catch (error) {
+        return {
+            success: false,
+            status: "😥 Something went wrong: " + error.message
+        }
+    }
+}
+
+export const connectWallet = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const obj = {
+        status: "👆🏽 Write a message in the text-field above.",
+        address: addressArray[0],
+      };
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download.html`}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const getCurrentWalletConnected = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: "👆🏽 Write a message in the text-field above.",
+        };
+      } else {
+        return {
+          address: "",
+          status: "🦊 Connect to Metamask using the top right button.",
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download.html`}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
